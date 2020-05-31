e14f744e30539e5b55811c48e8b7043a0e61729d
Jonathan Yates <carlock1609@gmail.com>
2020-05-31 10:41:24 -0700

    db hooked up, seaded with user

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Contact_Tracing', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 // pointing to location of routes
 const indexRoutes = require('./api/routes/index');
@@ -11,13 +17,26 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs'); // setting the view engine, were using ejs
 
+// seeding DB
+const User = require('./models/users');
+let testUser = {
+    email: 'carlock1609@SpeechGrammarList.com',
+    username: 'Carlock',
+    password: 'Yatesboy906',
+};
 
+User.create(testUser, function(err, createdUser) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Successfully created!' + createdUser);
+    }
+});
 
 
 
 
 app.use('/', indexRoutes);
-
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {

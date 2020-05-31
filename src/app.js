cf75147bb4bd300dac560c8969fedcd84955cd31
Jonathan Yates <carlock1609@gmail.com>
2020-05-30 22:47:26 -0700

    trying to set up background image

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 
 // pointing to location of routes
-const indexRoutes = require('../src/api/routes/index');
+const indexRoutes = require('./api/routes/index');
 
 // app settings
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public')); 
 
 app.set('view engine', 'ejs'); // setting the view engine, were using ejs
 
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs'); // setting the view engine, were using ejs
 app.use('/', indexRoutes);
 
 
-app.listen('8000', () => {
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
     console.log('SERVER IS UP!');
 });

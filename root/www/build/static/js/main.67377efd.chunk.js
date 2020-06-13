a6ded5f0348b79150c87c21be3170d4df86d48db
Jonathan Yates <carlock1609@gmail.com>
2020-06-13 11:07:15 -0700

    testing out 'npm run build'. This is for deploying

---
+++ root/www/build/static/js/main.67377efd.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwww=this.webpackJsonpwww||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var r=t(0),a=t.n(r),c=t(2),l=t.n(c),u=(t(8),function(){return a.a.createElement(r.Fragment,null,a.a.createElement("h1",null,"App"))});l.a.render(a.a.createElement(a.a.StrictMode,null,a.a.createElement(u,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.67377efd.chunk.js.map

c12fa169a0f584a9803ed982a4e2b959d272c93a
Jonathan Yates <carlock1609@gmail.com>
2020-06-14 18:40:26 -0700

    mongodb was messing up. index fixed

--- root/api/src/controllers/usersController.js
+++ root/api/src/controllers/usersController.js
@@ -9,7 +9,7 @@ const User = require('../models/User');
 // Register User - POST
 exports.register = async (req, res) => {
     const errors = validationResult(req);
-    console.log(req.body)
+
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }

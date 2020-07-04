b6b90579dbdb922be675ec5c51704082c700f987
Jonathan Yates <carlock1609@gmail.com>
2020-07-04 13:58:48 -0700

    styling journal form

--- root/www/src/components/dashboard-forms/addJournal.js
+++ root/www/src/components/dashboard-forms/addJournal.js
@@ -32,64 +32,68 @@ const AddJournal = ({ addJournal, history}) => {
 
     return (
         <Fragment>
-            <form
-                onSubmit={e => {
-                e.preventDefault();
-                addJournal(formData, history);
-                }}
-            >
-                <div id="journal_form">
-                    <div className="row">
-                    <h5 id="form_holder">Date/Time: </h5>
-                        <div className="col-md-6 col-sm-12 mb-1">
-                            <input type="date" name="date" value={date} onChange={e => onChange(e)} />
+            <div className="container">
+                <form
+                    onSubmit={e => {
+                    e.preventDefault();
+                    addJournal(formData, history);
+                    }}
+                >
+                    <div id="journal_form">
+                        <h5 id="form_holder">Date/Time: </h5>
+                        <div className="row">
+                            <div className="col-md-6 col-sm-12 mb-1">
+                                <input type="date" name="date" value={date} onChange={e => onChange(e)} />
+                            </div>
+                            <div className="col-md-6 col-sm-12 mb-1">
+                                <input type="time" name="time" value={time} onChange={e => onChange(e)} />
+                            </div>
                         </div>
-                        <div className="col-md-6 col-sm-12 mb-1">
-                            <input type="time" name="time" value={time} onChange={e => onChange(e)} />
+                        <h5 id="form_holder">Activity/Location: </h5>
+                        <div className="row">
+                            <div className="col-lg-6 col-md-6 mb-1">
+                                <input type="text" name="activity" value={activity} onChange={e => onChange(e)} placeholder="Activity here..." />
+                            </div>
+                            <div className="col-lg-6 col-md-6 mb-1">
+                                <input type="text" name="location" value={location} onChange={e => onChange(e)} placeholder="Location here..." />
+                            </div>
+                        </div>
+                            <h5 id="form_holder">Did you wear a mask: </h5>
+                        <div className="row">
+                            <div className="col-12 mb-1">
+                                <select name="choice1" value={choice1} onChange={e => onChange(e)} >
+                                    <option value="wore_mask">Wore Mask</option>
+                                    <option value="did_not">Did not wear Mask</option>
+                                </select>
+                            </div>
+                        </div>
+                        <h5 id="form_holder">Did others wear a mask: </h5>
+                        <div className="row">
+                            <div className="col-12 mb-1">
+                                <select name="choice2" value={choice2} onChange={e => onChange(e)} >
+                                    <option value="wore_mask">Wore Mask</option>
+                                    <option value="did_not">Did not wear Mask</option>
+                                </select>
+                            </div>
+                        </div>
+                        <h5 id="form_holder">Contact with COVID: </h5>
+                        <div className="row">
+                            <div className="col-12">
+                                <textarea type="text" name="contact" placeholder="Any contact with COVID here..." value={contact} onChange={e => onChange(e)} ></textarea>
+                            </div>
+                        </div>
+                        <h5 id="form_holder">Other notes: </h5>
+                        <div className="row mb-1">
+                            <div className="col-12">
+                                <textarea type="text" name="notes"  placeholder="Any notes here..." value={notes} onChange={e => onChange(e)} ></textarea>
+                            </div>
+                        </div>
+                        <div className="text-center">
+                            <button className="btn btn-outline-dark" type="submit">Submit</button>
                         </div>
                     </div>
-                    <div className="row">
-                    <h5 id="form_holder">Activity/Location: </h5>
-                        <div className="col-lg-6 col-md-6 mb-1">
-                            <input type="text" name="activity" value={activity} onChange={e => onChange(e)} placeholder="Activity here..." />
-                        </div>
-                        <div className="col-lg-6 col-md-6 mb-1">
-                            <input type="text" name="location" value={location} onChange={e => onChange(e)} placeholder="Location here..." />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <h5 id="form_holder">Did you wear a mask: </h5>
-                        <div className="col-12 mb-1">
-                            <select name="choice1" value={choice1} onChange={e => onChange(e)} >
-                                <option value="wore_mask">Wore Mask</option>
-                                <option value="did_not">Did not wear Mask</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="row">
-                    <h5 id="form_holder">Did others wear a mask: </h5>
-                        <div className="col-12 mb-1">
-                            <select name="choice2" value={choice2} onChange={e => onChange(e)} >
-                                <option value="wore_mask">Wore Mask</option>
-                                <option value="did_not">Did not wear Mask</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-12">
-                            <textarea type="text" name="contact" placeholder="Any contact with COVID here..." value={contact} onChange={e => onChange(e)} ></textarea>
-                        </div>
-                    </div>
-                    <div className="row mb-1">
-                        <div className="col-12">
-                            <textarea type="text" name="notes"  placeholder="Any notes here..." value={notes} onChange={e => onChange(e)} ></textarea>
-                        </div>
-                    </div>
-                    <div className="text-center">
-                        <button className="btn btn-outline-dark" type="submit">Submit</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </Fragment>
     )
 };
--- root/www/src/components/layout/ContactUs.js
+++ root/www/src/components/layout/ContactUs.js
@@ -5,7 +5,7 @@ const ContactUs = (props) => {
         <div>
             <h3 className="mt-5">CONTACT US</h3>
             <div className="container mt-5">
-                <h5>EMAIL: </h5>
+                <h5>EMAIL: development@email.com</h5>
                 <h5>PHONE: 1-800-379-1511</h5>
                 <a id="facebook_link" href="https://www.facebook.com/US-together-104235761324547/?view_public_for=104235761324547">
                     <i className="fab fa-facebook"></i>
